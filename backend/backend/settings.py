2b1a25717240d45a011f4ed5a6c96364edf9550e
YashPatkar <yash.patkar2004@gmail.com>
2026-01-02 17:15:16 +0530

    Added cors header to allow all

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,11 +46,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
-print("CORS_ALLOWED_ORIGINS:", CORS_ALLOWED_ORIGINS)
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+# print("CORS_ALLOWED_ORIGINS:", CORS_ALLOWED_ORIGINS)
+# CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+# CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = ["https://*.vercel.app"]
 
 from corsheaders.defaults import default_headers
 

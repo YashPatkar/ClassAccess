190e9503ac660d5527647f688b66b9600fb3feb4
YashPatkar <yash.patkar2004@gmail.com>
2026-01-04 15:19:26 +0530

    fixed migrations and custom user code

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
 class CustomManager(BaseUserManager):
-    def create_user(self, email, password=None, role='student', **extra_fields):
-        user = self.model(email=email, role=role.lower(), **extra_fields)
+    def create_user(self, email, password=None, role="student", **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
-    
+
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -16,19 +20,20 @@ class CustomManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
 
+
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
-    
-    USERNAME_FIELD = 'email'
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     ROLE_CHOICES = [
-        ('admin', 'admin'),
-        ('teacher', 'teacher'),
-        ('student', 'student')
+        ("admin", "admin"),
+        ("teacher", "teacher"),
+        ("student", "student"),
     ]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    REQUIRED_FIELDS = ['role']
 
     objects = CustomManager()
-

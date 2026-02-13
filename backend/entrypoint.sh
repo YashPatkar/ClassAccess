47beff0dd6a75dce3cf046ba9e04d10b7873c0ea
YashPatkar <yash.patkar2004@gmail.com>
2026-02-13 22:00:55 +0530

    Added whitenose

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@ echo "DATABASE_URL=$DATABASE_URL"
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Starting gunicorn..."
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:8000
